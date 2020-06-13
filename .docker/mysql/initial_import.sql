d2b710261591d2062080d169212db8bc63c98662
Gorazd Veselič <gorazd@hurra-studios.com>
2020-06-13 11:51:03 +0200

    Initial commit

---
+++ .docker/mysql/initial_import.sql
@@ -0,0 +1,12 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : localhost
+Source Database       : my_database
+
+Target Server Type    : MYSQL
+
+*/
+
+CREATE DATABASE IF NOT EXISTS `my_database` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `my_database`;
